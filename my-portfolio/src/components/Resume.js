375e97c801789604775ab36a6c0e89833871e6fd
Vijay Kumar <saroyav3@gmail.com>
2024-07-01 17:14:09 +1200

    minor change

--- my-portfolio/src/components/Resume.js
+++ my-portfolio/src/components/Resume.js
@@ -49,15 +49,15 @@ const Resume = () => {
         <div className="resume__section">
           <h2>Experience</h2>
           <div>
-            <h3>IT Service-Desk Analyst – Ranui Action Project</h3>
+            <h3>IT Service-Desk Analyst – Ranui Action Project (May 24 - Current)</h3>
             <p>May 2024-C</p>
             <ul>
               <li>Provided Tier 1 technical support to employees, customers, and clients, resolving issues related to software, hardware, and network connectivity.</li>
             </ul>
           </div>
           <div>
-            <h3>Software Developer (Intern) – Arahura Trust</h3>
-            <p>Nov 2020 - Mar 2021</p>
+            <h3>Software Developer Intern  – Arahura Trust (Nov 2020 - Mar 2021)</h3>
+            <p></p>
             <ul>
               <li>Developed a centralized patient notes system using MS Access and VB, consolidating records and forms into a single, accessible database.</li>
               <li>Reduced data entry time by 50% and improved data accuracy by 40%.</li>
@@ -66,22 +66,22 @@ const Resume = () => {
           </div>
           
           <div>
-            <h3>Community Support Worker – Walsh Trust</h3>
-            <p>Jun 2017-C</p>
+            <h3>Community Support Worker – Walsh Trust (July 17 - Current)</h3>
+           
           </div>
         </div>
         <div className="resume__section">
           <h2>Education</h2>
           <div>
-            <h3>BS Computer Science – Unitec New Zealand</h3>
-            <p>Nov 2024</p>
+            <h3>BS Computer Science – Unitec New Zealand (Nov 2024)</h3>
+            
             <ul>
               <li>Relevant coursework: Data Analytics and Intelligence, Data Warehousing, Database design and development, Introduction to Databases, Agile and Lean software delivery, Programming principles and fundamentals, Project planning and control, Cloud design and development, and Professional skills for IT practitioners.</li>
             </ul>
           </div>
           <div>
-            <h3>Diploma in Health-Care Management – AGI</h3>
-            <p>Nov 2015</p>
+            <h3>Diploma in Health-Care Management – AGI (Nov 2015)</h3>
+           
             <ul>
               <li>Relevant coursework: 530 Organization and Management, 560 Business Communication and 630 Leadership and 636 Applied management.</li>
             </ul>
