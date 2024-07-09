e3a6d675ffec191bcb3be77ad8201094e5ca8b8d
Vijay Kumar <saroyav3@gmail.com>
2024-07-09 21:32:04 +1200

    adding new project

--- my-portfolio/src/components/Projects.js
+++ my-portfolio/src/components/Projects.js
@@ -9,9 +9,24 @@ const Projects = () => {
     <section className="projects">
       <h1>Projects</h1>
       <div className="projects__content">
+      <div className="projects__item">
+          <h2>MS Fabric Bing News</h2>
+          <p>This project aims to develop a machine learning model to predict the prices of used Toyota cars in California. By leveraging a Random Forest regression model, the solution provides accurate price predictions based on car model, year, and mileage. The project also includes an interactive dashboard for users to input car details and receive price predictions, along with a summary of the car inventory.</p>
+          <Carousel showThumbs={false}>
+            <div>
+              <img src="design.png" alt="Project 2 Image 1" />
+            </div>
+            <div>
+              <img src="dashboard.png" alt="Project 2 Image 2" />
+            </div>
+            <div>
+              <img src="actualvspredicted.png" alt="Project 2 Image 3" />
+            </div>
+          </Carousel>
+        </div>
         <div className="projects__item">
           <h2>Quiz Time - Android Mobile Application</h2>
-          <p>Welcome to the Quiz Tournament App! This is a platform where users can create, manage, and participate in quiz tournaments. There are two types of users: Admin and Player. Admin users have the ability to create and manage tournaments, while Player users can participate in tournaments and interact with them.</p>
+          <p>a platform where users can create, manage, and participate in quiz tournaments. There are two types of users: Admin and Player. Admin users have the ability to create and manage tournaments, while Player users can participate in tournaments and interact with them.</p>
           <Carousel showThumbs={false}>
             <div>
               <img src="img4.png" alt="Project 1 Image 1" />
@@ -40,21 +55,7 @@ const Projects = () => {
             </div>
           </Carousel>
         </div>
-        <div className="projects__item">
-          <h2>ABI Solution: Predict price of used cars</h2>
-          <p>This project aims to develop a machine learning model to predict the prices of used Toyota cars in California. By leveraging a Random Forest regression model, the solution provides accurate price predictions based on car model, year, and mileage. The project also includes an interactive dashboard for users to input car details and receive price predictions, along with a summary of the car inventory.</p>
-          <Carousel showThumbs={false}>
-            <div>
-              <img src="design.png" alt="Project 2 Image 1" />
-            </div>
-            <div>
-              <img src="dashboard.png" alt="Project 2 Image 2" />
-            </div>
-            <div>
-              <img src="actualvspredicted.png" alt="Project 2 Image 3" />
-            </div>
-          </Carousel>
-        </div>
+
 
         {/* Add more projects as needed */}
       </div>
