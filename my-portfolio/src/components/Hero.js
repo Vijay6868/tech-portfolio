ea8e228989f4beb3724e781ed7851952694f9eba
Vijay Kumar <saroyav3@gmail.com>
2024-07-12 14:27:36 +1200

    badges added on home page

--- my-portfolio/src/components/Hero.js
+++ my-portfolio/src/components/Hero.js
@@ -1,4 +1,3 @@
-// src/components/Hero.js
 import React from 'react';
 import './Hero.css';
 
@@ -10,11 +9,15 @@ const Hero = () => {
         <h1>Hello</h1>
         <h2>A Bit About Me</h2>
         <p>
-        Welcome to my site, I'm a dedicated professional specializing in Data and Software Development.
-         As a certified Azure AI Engineer and Data Engineer, 
-         I leverage technology to solve real-world problems and deliver innovative solutions. 
-         My projects reflect my commitment to quality and excellence. I’m always eager to connect and explore new opportunities in the tech industry!
+          Welcome to my site, I'm a dedicated professional specializing in Data and Software Development.
+          As a certified Azure AI Engineer and Data Engineer,
+          I leverage technology to solve real-world problems and deliver innovative solutions.
+          My projects reflect my commitment to quality and excellence. I’m always eager to connect and explore new opportunities in the tech industry!
         </p>
+        <div className="badges">
+          <img src={`${process.env.PUBLIC_URL}/azure_ai.png`} alt="AI Engineer Badge" className="badge" />
+          <img src={`${process.env.PUBLIC_URL}/azure_data.png`} alt="Data Engineer Badge" className="badge" />
+        </div>
       </div>
     </section>
   );
