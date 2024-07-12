36780fe0010abbbac3089417fbe3048fb7e8d733
Vijay Kumar <saroyav3@gmail.com>
2024-07-12 13:57:46 +1200

    updated

--- my-portfolio/src/components/Hero.js
+++ my-portfolio/src/components/Hero.js
@@ -10,7 +10,10 @@ const Hero = () => {
         <h1>Hello</h1>
         <h2>A Bit About Me</h2>
         <p>
-        Welcome to my site, a dedicated professional specializing in Data and Software Development. As a certified Azure AI Engineer and Data Engineer, I leverage technology to solve real-world problems and deliver innovative solutions. My projects reflect my commitment to quality and excellence. I’m always eager to connect and explore new opportunities in the tech industry!
+        Welcome to my site, I'm a dedicated professional specializing in Data and Software Development.
+         As a certified Azure AI Engineer and Data Engineer, 
+         I leverage technology to solve real-world problems and deliver innovative solutions. 
+         My projects reflect my commitment to quality and excellence. I’m always eager to connect and explore new opportunities in the tech industry!
         </p>
       </div>
     </section>
--- my-portfolio/src/components/Projects.js
+++ my-portfolio/src/components/Projects.js
@@ -11,10 +11,18 @@ const Projects = () => {
       <div className="projects__content">
       <div className="projects__item">
           <h2>MS Fabric Bing News</h2>
-          <p>This is a Data Engineering project</p>
+          <p>This project demonstrates a comprehensive data pipeline, 
+            combining data engineering with data analysis and visualization. 
+            The pipeline processes bing news data in JSON format, 
+            performs transformations using PySpark, saves the data in Delta tables, 
+            loads the data for sentiment analysis, and visualizes insights using a Power BI dashboard.</p>
+            
           <Carousel showThumbs={false}>
             <div>
               <img src="dash2.png" alt="Project 2 Image 1" />
+            </div>
+            <div>
+              <img src="pipeline.png" alt="Project 2 Image 2" />
             </div>
             <div>
               <img src="fabric1.png" alt="Project 2 Image 2" />
@@ -26,7 +34,10 @@ const Projects = () => {
         </div>
         <div className="projects__item">
           <h2>Quiz Time - Android Mobile Application</h2>
-          <p>a platform where users can create, manage, and participate in quiz tournaments. There are two types of users: Admin and Player. Admin users have the ability to create and manage tournaments, while Player users can participate in tournaments and interact with them.</p>
+          <p>A platform where users can create, manage, and participate in quiz tournaments. 
+            There are two types of users: Admin and Player. 
+            Admin users have the ability to create and manage tournaments, 
+            while Player users can participate in tournaments and interact with them.</p>
           <Carousel showThumbs={false}>
             <div>
               <img src="img4.png" alt="Project 1 Image 1" />
@@ -42,7 +53,11 @@ const Projects = () => {
 
         <div className="projects__item">
           <h2>ABI Solution: Predict price of used cars</h2>
-          <p>This project aims to develop a machine learning model to predict the prices of used Toyota cars in California. By leveraging a Random Forest regression model, the solution provides accurate price predictions based on car model, year, and mileage. The project also includes an interactive dashboard for users to input car details and receive price predictions, along with a summary of the car inventory.</p>
+          <p>This project aims to develop a machine learning model to predict the prices of used Toyota cars in California. 
+            By leveraging a Random Forest regression model, 
+            the solution provides accurate price predictions based on car model, year, and mileage. 
+            The project also includes an interactive dashboard for users to input car details and receive price predictions, 
+            along with a summary of the car inventory.</p>
           <Carousel showThumbs={false}>
             <div>
               <img src="design.png" alt="Project 2 design" />
