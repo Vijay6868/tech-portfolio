c150a3d26f3b0d188340e0b492af562ccadebbeb
Vijay Kumar <saroyav3@gmail.com>
2024-07-30 10:42:24 +1200

    projects added

--- my-portfolio/src/components/Projects.js
+++ my-portfolio/src/components/Projects.js
@@ -10,6 +10,41 @@ const Projects = () => {
       <h1>Projects</h1>
       <div className="projects__content">
       <div className="projects__item">
+          <h2>Ecommerce Sales</h2>
+          <p>The goal of this project is to showcase how e-commerce data can be processed using
+             a Medallion Architecture, leading to a comprehensive reporting solution in Power BI.</p>
+            
+          <Carousel showThumbs={false}>
+            <div>
+              <img src="Ecommerce/1.png" alt="Project 2 Image 1" />
+            </div>
+            <div>
+              <img src="Ecommerce/2.png" alt="Project 2 Image 2" />
+            </div>
+            <div>
+              <img src="Ecommerce/3.png" alt="Project 2 Image 2" />
+            </div>
+            <div>
+              <img src="Ecommerce/4.png" alt="Project 2 Image 3" />
+            </div>
+          </Carousel>
+        </div>
+        
+
+        <div className="projects__item">
+          <h2>QuoteWell - An Android Mobile Application</h2>
+          <p>QuoteWell is an Android application that allows users to fetch and display quotes based on a specified keyword. 
+            It utilizes the ZenQuotes API for fetching quotes.</p>
+            
+          <Carousel showThumbs={false}>
+            <div>
+              <img src="quoteWellwide.png" alt="Project 2 Image 1" />
+            </div>
+          </Carousel>
+        </div>
+
+
+        <div className="projects__item">
           <h2>MS Fabric Bing News</h2>
           <p>This project demonstrates a comprehensive data pipeline, 
             combining data engineering with data analysis and visualization. 
@@ -32,6 +67,7 @@ const Projects = () => {
             </div>
           </Carousel>
         </div>
+
         <div className="projects__item">
           <h2>Earthquake Events</h2>
           <p>This project demonstrates an end-to-end data engineering solution using the 
