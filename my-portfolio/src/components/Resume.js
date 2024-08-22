40a64bb2408ebc5cef99d3b08849a51bd21a6104
Vijay Kumar <saroyav3@gmail.com>
2024-08-22 16:01:33 +1200

    resume updated

--- my-portfolio/src/components/Resume.js
+++ my-portfolio/src/components/Resume.js
@@ -24,7 +24,7 @@ const Resume = () => {
           <ul>
             <li>Azure AI Engineer Associate</li>
             <li>Azure Data Engineer Associate</li>
-            <li>AWS Cloud Practitioner</li>
+            
           </ul>
         </div>
         <div className="resume__section">
@@ -32,8 +32,8 @@ const Resume = () => {
           <ul>
             <li>Auckland, New Zealand</li>
             <li>saroyav3@gmail.com</li>
-            <li><a href="https://www.linkedin.com/in/vijay68" target="_blank" rel="noopener noreferrer">linkedin.com/in/vijay68</a></li>
-            <li><a href="https://github.com/Vijay6868" target="_blank" rel="noopener noreferrer">github.com/Vijay6868</a></li>
+            <li><a href="https://www.linkedin.com/in/vj68" target="_blank" rel="noopener noreferrer">Linkedin</a></li>
+            <li><a href="https://github.com/Vijay6868" target="_blank" rel="noopener noreferrer">GitHub</a></li>
           </ul>
         </div>
         <div className="resume__section">
@@ -46,19 +46,29 @@ const Resume = () => {
       </div>
       <div className="resume__right">
         <div className="resume__section">
+          <h2>Objective</h2>
+          <p>Motivated and passionate Final-year Engineering student specializing in Computer Science 
+            with hands-on experience in Software and Data Engineering. My coursework and hands-on projects 
+            in programming languages, web technologies, data engineering, and cloud computing have prepared
+             me to contribute effectively to dynamic development teams. I am keen to apply my passion for 
+             technology and problem-solving in a professional setting, aiming to drive innovation and deliver 
+             high-quality solutions.</p>
+        </div>
+        <div className="resume__section">
           <h2>Experience</h2>
           <div>
             <h3>IT Service-Desk Analyst – Ranui Action Project (May 24 - Current)</h3>
-            <p>May 2024-C</p>
+            
             <ul>
-              <li>Provided Tier 1 technical support to employees, customers, and clients, resolving issues related to software, hardware, and network connectivity.</li>
+              <li>Provided Tier 1 technical support to RAP employees and clients.</li>
+              <li>Resolve issues related to software, hardware, and network connectivity. </li>
             </ul>
           </div>
           <div>
-            <h3>Software Developer Intern  – Arahura Trust (Nov 2020 - Mar 2021)</h3>
+            <h3>Software Developer Intern  – Arahura Trust (Nov 2020 - Aug 2021)</h3>
             <p></p>
             <ul>
-              <li>Developed a centralized patient notes system using MS Access and VB, consolidating records and forms into a single, accessible database.</li>
+              <li>Developed Patient Management System records to record Patient's Progress</li>
               <li>Reduced data entry time by 50% and improved data accuracy by 40%.</li>
               <li>Streamlined workflows, enabling a 25% increase in patient load management.</li>
             </ul>
@@ -74,18 +84,46 @@ const Resume = () => {
           <div>
             <h3>BS Computer Science – Unitec New Zealand (Nov 2024)</h3>
             
+            <div className="resume__coursework">
             <ul>
-              <li>Relevant coursework: Data Analytics and Intelligence, Data Warehousing, Database design and development, Introduction to Databases, Agile and Lean software delivery, Programming principles and fundamentals, Project planning and control, Cloud design and development, and Professional skills for IT practitioners.</li>
+              <li>Internet and Website Development</li>
+              <li>Programming Principle & Practice</li>
+              <li>Mobile Software Development</li>
+              <li>GUI Programming</li>
+              <li>Data Structure & Algorithms</li>
+              <li>Programming Fundamentals</li>
+              <li>Agile & Lean Software Delivery</li>
             </ul>
+            <ul>
+              <li>Database Design & Development</li>
+              <li>Data Warehousing</li>
+              <li>Data Analytics and Intelligence</li>
+              <li>Introduction to Databases</li>
+              <li>Network Operating System</li>
+              <li>Networking Fundamentals</li>
+            </ul>
+            <ul>
+              <li>Professional IT Skills</li>
+              <li>System Analysis & Design</li>
+              <li>Information System Concepts</li>
+              <li>Cloud Design & Development</li>
+              <li>Project Planning & Control</li>
+              <li>OS Fundamentals</li>
+            </ul>
+          </div>
           </div>
           <div>
             <h3>Diploma in Health-Care Management – AGI (Nov 2015)</h3>
            
             <ul>
-              <li>Relevant coursework: 530 Organization and Management, 560 Business Communication and 630 Leadership and 636 Applied management.</li>
+              <li>530 Organization and Management</li> 
+              <li>560 Business Communication</li>
+              <li>630 Leadership</li>
+              <li>636 Applied management</li>
             </ul>
           </div>
         </div>
+        
       </div>
     </section>
   );
